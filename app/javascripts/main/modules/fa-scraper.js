4fbf73fda562ebc14b1aeae8fa2266b7dacec570
SudoBun <sudobun@gmail.com>
2017-03-29 00:21:31 -0700

    Scrape FA theme type

--- app/javascripts/main/modules/fa-scraper.js
+++ app/javascripts/main/modules/fa-scraper.js
@@ -8,21 +8,34 @@ class FaScraper {
 			show: true,
 			electronPath: require('electron-prebuilt')
 		})
-		
-		this.nightmare.goto('https://duckduckgo.com')
-			.type('#search_form_input_homepage', 'github nightmare')
-			.click('#search_button_homepage')
-			.wait('#zero_click_wrapper .c-info__title a')
-			.evaluate(function () {
-				return document.querySelector('#zero_click_wrapper .c-info__title a').href;
+			
+		this.nightmare.goto('https://furaffinity.net')
+			.wait('body')
+			.evaluate(function() {
+				return STATIC_PATH
 			})
 			.end()
 			.then(function (result) {
-				console.log(result);
+				console.log(result)
 			})
 			.catch(function (error) {
-				console.error('Search failed:', error);
+				console.error('Scrape failed:', error)
 			})
+
+//		this.nightmare.goto('https://duckduckgo.com')
+//			.type('#search_form_input_homepage', 'github nightmare')
+//			.click('#search_button_homepage')
+//			.wait('#zero_click_wrapper .c-info__title a')
+//			.evaluate(function () {
+//				return document.querySelector('#zero_click_wrapper .c-info__title a').href
+//			})
+//			.end()
+//			.then(function (result) {
+//				console.log(result)
+//			})
+//			.catch(function (error) {
+//				console.error('Search failed:', error)
+//			})
 	}
 }
 
